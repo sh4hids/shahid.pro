aef443140eb5e1e17c64e6c51a0ab029826db7a9
Shahidul Islam Majumder <dev@shahid.pro>
2024-05-11 20:33:22 +0600

    feat: add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { remarkPostMeta } from './src/utils/remark-post-meta.mjs';
-
 import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://shahid.pro/',
+    site: 'https://shahid.pro',
     integrations: [
         tailwind({
             applyBaseStyles: false,
         }),
         react(),
+        sitemap(),
     ],
     markdown: {
         remarkPlugins: [remarkPostMeta],
     },
 });
+
