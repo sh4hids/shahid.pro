3b1088fac88d874a213f2f6cdd6427642a723f8b
Shahidul Islam Majumder <dev@shahid.pro>
2024-05-17 10:48:59 +0600

    feat: add mobile menu

--- src/config.ts
+++ src/config.ts
@@ -19,18 +19,12 @@ export type SiteMeta = {
 };
 
 export const siteConfig: SiteConfig = {
-    // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
     author: 'Shahidul Islam Majumder',
-    // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
     title: 'Shahidul Islam Majumder',
-    // Meta property used as the default description meta property
     description:
-        'Portfolio website of Shahidul Islam Majumder, a full stack software engineer from Feni, Bangladesh.',
-    // HTML lang property, found in src/layouts/Base.astro L:18
+        'Portfolio website of Shahidul Islam Majumder, a fullstack software engineer from Feni, Bangladesh.',
     lang: 'en',
-    // Meta property, found in src/components/BaseHead.astro L:42
     ogLocale: 'en_US',
-    // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
     date: {
         locale: 'en-US',
         options: {
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,6 +18,15 @@ export default {
     },
     theme: {
         extend: {
+            keyframes: {
+                'fade-in': {
+                    '0%': { opacity: '0%' },
+                    '100%': { opacity: '100%' },
+                },
+            },
+            animation: {
+                'fade-in': 'fade-in 0.2s ease-in-out',
+            },
             typography: () => ({
                 DEFAULT: {
                     css: {
